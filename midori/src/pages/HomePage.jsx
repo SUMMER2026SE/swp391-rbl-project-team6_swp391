93e1f7282f8a77e09aba55aed137c989ee533916
nguyendinhdat210920-lab <nguyendinhdat210920@gmail.com>
2026-05-21 15:36:48 +0700

    Initial project setup

---
+++ midori/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const HomePage = () => {
+  return (
+    <Container className="mt-5 text-center">
+      <h1>Chào mừng đến với Midori</h1>
+    </Container>
+  );
+};
+
+export default HomePage;
