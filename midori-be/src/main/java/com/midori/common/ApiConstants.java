3053931aefa7867c4ce4e396cb425452a4477309
Pham anh <phamanh20102005@gmail.com>
2026-06-03 16:40:23 +0700

    API Constants
    Contains application-wide API constants and fixed values used throughout the system.

---
+++ midori-be/src/main/java/com/midori/common/ApiConstants.java
@@ -0,0 +1,11 @@
+package com.midori.common;
+
+public class ApiConstants {
+    public static final String BASE_URL = "/api";
+    public static final String AUTH_URL = BASE_URL + "/auth";
+    public static final String USERS_URL = BASE_URL + "/users";
+    public static final String PROFILES_URL = BASE_URL + "/profiles";
+
+    private ApiConstants() {
+    }
+}
