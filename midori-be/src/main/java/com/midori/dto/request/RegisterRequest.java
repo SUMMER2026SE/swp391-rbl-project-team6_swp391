9eb3533365550a70ac5545aae4bd6d30e80369c3
nnngann <rannt67@gmail.com>
2026-06-03 16:45:56 +0700

    add authentication request DTOs

---
+++ midori-be/src/main/java/com/midori/dto/request/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.midori.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Size(max = 255, message = "Email must not exceed 255 characters")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&.#_\\-])[A-Za-z\\d@$!%*?&.#_\\-]{8,}$",
+            message = "Password must be at least 8 characters and include uppercase, lowercase, number, and special character"
+    )
+    private String password;
+
+    private String role;
+}
