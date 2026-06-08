35d06b0f43e9ff971a4c5195e6dc922cd9ef8461
myle0901-ntml <myle1905333@gmail.com>
2026-06-08 19:49:49 +0700

    Connect Teacher Vocabulary detail

--- midori-be/src/main/java/com/midori/exception/GlobalExceptionHandler.java
+++ midori-be/src/main/java/com/midori/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.midori.exception;
 
 import com.midori.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -50,6 +52,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGenericException(Exception ex) {
+        log.error("Unhandled exception in request", ex);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error("An unexpected error occurred: " + ex.getMessage()));
