13c1fa7dfb573326937b284ddc351abc12694899
myle0901-ntml <myle1905333@gmail.com>
2026-06-18 22:49:40 +0700

    delete

--- midori-fe/src/routes/student.practice.flashcards.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/student/practice/flashcards")({
-  beforeLoad: () => {
-    throw redirect({ to: "/student/flashcards" });
-  },
-});
