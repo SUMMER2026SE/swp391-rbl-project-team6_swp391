1999c369e227bb6d68509935360e682ead830546
myle0901-ntml <myle1905333@gmail.com>
2026-06-18 22:51:41 +0700

    delete

--- midori-fe/src/routes/student.assignments.homework.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AssignmentsDashboard } from "@/components/assignments-dashboard";
-
-export const Route = createFileRoute("/student/assignments/homework")({
-  component: () => <AssignmentsDashboard activeTab="homework" />,
-});
