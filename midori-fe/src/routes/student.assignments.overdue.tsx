085bfdb96bd19d2d8ba1e49289429cf321e243d4
myle0901-ntml <myle1905333@gmail.com>
2026-06-18 22:51:38 +0700

    delete

--- midori-fe/src/routes/student.assignments.overdue.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AssignmentsDashboard } from "@/components/assignments-dashboard";
-
-export const Route = createFileRoute("/student/assignments/overdue")({
-  component: () => <AssignmentsDashboard activeTab="overdue" />,
-});
