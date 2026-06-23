da9fb6d9df6a94592dbdfc168deea8785d331bb1
nnngann <rannt67@gmail.com>
2026-06-24 01:04:33 +0700

    kanjiDTO

---
+++ midori-be/src/main/java/com/midori/dto/kanji/KanjiDTO.java
@@ -0,0 +1,19 @@
+package com.midori.dto.kanji;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KanjiDTO {
+    private String character;
+    private String hanViet;
+    private String meaning;
+    private List<String> strokeOrderImages;
+}
