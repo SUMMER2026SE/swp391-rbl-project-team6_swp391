bc4068dd84034bf06de29c2c4520596b08b7b0ca
myle0901-ntml <myle1905333@gmail.com>
2026-07-09 22:43:19 +0700

    sửa

--- midori-be/src/main/resources/db/migration/V16__fix_shadowing_sentences_id.sql
+++
@@ -1,9 +0,0 @@
--- Fix shadowing_sentences: add DEFAULT for UUID generation
--- This is required because @GeneratedValue(strategy = GenerationType.UUID)
--- in Hibernate 6 needs a database-side DEFAULT on PostgreSQL.
-
-ALTER TABLE shadowing_sentences
-ALTER COLUMN id SET DEFAULT gen_random_uuid();
-
-ALTER TABLE shadowing_sentences
-ALTER COLUMN id DROP NOT NULL;
