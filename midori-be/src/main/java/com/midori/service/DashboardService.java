4dc57ea9e63392b4b1c0c6d948e8c92fc33067aa
Pham anh <phamanh20102005@gmail.com>
2026-07-16 13:21:35 +0700

    Interface dashboard

--- midori-be/src/main/java/com/midori/service/DashboardService.java
+++ midori-be/src/main/java/com/midori/service/DashboardService.java
@@ -1,8 +1,14 @@
 package com.midori.service;
 
 import com.midori.dto.response.AdminDashboardSummaryResponse;
+import com.midori.dto.response.JlptDistributionResponse;
+import com.midori.dto.response.RecentActivitiesResponse;
 
 public interface DashboardService {
 
     AdminDashboardSummaryResponse getSummary();
+
+    JlptDistributionResponse getJlptDistribution();
+
+    RecentActivitiesResponse getRecentActivities(int limit);
 }
